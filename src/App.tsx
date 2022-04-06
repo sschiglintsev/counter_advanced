fbc81ce92b1709f17924ce82db0b3e8e0e6e2eeb
sschiglintsev <s.s.chiglintsev@gmail.com>
2022-04-07 01:12:35 +0500

    css

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import c from './App.module.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className={c.App}>
+            <div className={c.Up}>
+                COUNTER
+            </div>
+            <div className={c.Window}>
+                <div className={c.Window_min_max}>
+                    <input/>
+                    <button>MIN</button>
+                </div>
+                <div className={c.Window_min_max}>
+                    <input/>
+                    <button>MAX</button>
+                </div>
+            </div>
+            <div className={c.Window}>
+                <div className={c.Window_min_max}>
+                    <input/>
+                </div>
+                <div className={c.Window_min_max}>
+                    <button>INC</button>
+                    <button>SET</button>
+                </div>
+            </div>
+
+        </div>
+
+    )
 }
 
 export default App;
